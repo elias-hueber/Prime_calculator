d173d4954022eacf9a534ca3635453df4d0806bb
elias-hueber <123167700+elias-hueber@users.noreply.github.com>
2023-01-20 13:30:49 +0100

    Update Sieb_des_Eratosthenes.py

--- Sieb_des_Eratosthenes.py
+++ Sieb_des_Eratosthenes.py
@@ -2,18 +2,20 @@ prime = []
 x = 0
 y = 2
 result = 0
-for i in range(2, 1001):
+end = 1000
+target = 100
+for i in range(2, end + 1):
     prime.append(i)
 while True:
-    while result <= 1000:
+    while result <= end:
         result = prime[x] * y
         y += 1
         if result in set(prime):
             prime.remove(result)
-    if prime[x] * prime[x] > 1000:
+    if prime[x] * prime[x] > end:
         break
     x += 1
     y = 2
     result = 0
 
-print(prime[99])
+print(prime[target - 1])
